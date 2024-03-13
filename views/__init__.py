b428e3631ca5afd6c2d5714a2b8a9c5b93a638bd
Eric Heidel <ericleeheidel@yahoo.com>
2024-03-13 10:16:46 -0500

    do_DELETE delete_tag() created

--- views/__init__.py
+++ views/__init__.py
@@ -15,4 +15,4 @@ from .post import (
 )
 from .categories import get_categories, create_category, delete_category, edit_category
 from .comment import get_comments_by_post_id, create_comment
-from .tags import create_tag, add_tags_to_post, get_tags
+from .tags import create_tag, add_tags_to_post, get_tags, delete_tag
