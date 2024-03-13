c663323c75823cd5ccf7d8f5ca67378361ec0db8
Eric Heidel <ericleeheidel@yahoo.com>
2024-03-13 10:26:31 -0500

    delete_tag() self tested ready for peer testing

--- views/tags.py
+++ views/tags.py
@@ -61,7 +61,7 @@ def get_tags():
 
 
 def delete_tag(pk):
-    with sqlite3.connect("./db/sqlite3") as conn:
+    with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
